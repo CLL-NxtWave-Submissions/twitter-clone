392232177c22a0d0e46df494a178860bb4c33be4
Surya T N <suryatej.nim@yahoo.com>
2022-08-25 08:03:32 +0000

    Implemented server-side handler for client requests to API end-point: POST /register, and also helper methods: validateUsername, validatePassword

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const jwt = require("jsonwebtoken");
 const app = express();
 app.use(express.json());
 
+const SALT_ROUNDS_FOR_PASSWORD_HASHING = 10;
+
 const twitterCloneDatabaseFilePath = path.join(__dirname, "twitterClone.db");
 const sqliteDriver = sqlite3.Database;
 
@@ -31,5 +33,114 @@ const initializeDBAndServer = async () => {
 };
 
 initializeDBAndServer();
+
+/*
+    Function Name   : validateUsername
+    Input Parameter : inputUsername
+    Return Value    : Validation Result Object
+        - isNewUser : Boolean true for new user
+                      and false otherwise
+        - failedMsg : Failed validation message
+    --------------------------------------------
+    Description: Function to validate input
+                 username and accordingly
+                 return the result in an object.
+*/
+const validateUsername = async (inputUsername) => {
+  let validationResult = {
+    isNewUser: true,
+    failedMsg: "",
+  };
+
+  const queryToFetchExistingUserData = `
+    SELECT
+        *
+    FROM
+        user
+    WHERE
+        username = '${inputUsername}';
+    `;
+
+  const existingUserData = await twitterCloneDBConnectionObj.get(
+    queryToFetchExistingUserData
+  );
+  if (existingUserData !== undefined) {
+    validationResult.isNewUser = false;
+    validationResult.failedMsg = "User already exists";
+  }
+
+  return validationResult;
+};
+
+/*
+    Function Name         : validatePassword
+    Input Parameter       : inputPassword
+    Return Value          : Validation Result Object
+        - isValidPassword : Boolean true for valid
+                            password and false otherwise
+        - failedMsg       : Failed validation message
+    -----------------------------------------------------
+    Description: Function to validate input
+                 password and accordingly
+                 return the result in an object.
+*/
+const validatePassword = (inputPassword) => {
+  let validationResult = {
+    isValidPassword: true,
+    failedMsg: "",
+  };
+
+  if (inputPassword.length < 6) {
+    validationResult.isValidPassword = false;
+    validationResult.failedMsg = "Password is too short";
+  }
+
+  return validationResult;
+};
+
+/*
+    End-Point 1: POST /register
+    ------------
+    To register/add new user
+    to the user table with
+    checks in place to validate
+    input username and password
+*/
+app.post("/register", async (req, res) => {
+  const { username, password, name, gender } = req.body;
+
+  const usernameValidationResult = await validateUsername(username);
+
+  if (!usernameValidationResult.isNewUser) {
+    res.status(400);
+    res.send(usernameValidationResult.failedMsg);
+  } else {
+    const passwordValidationResult = validatePassword(password);
+
+    if (!passwordValidationResult.isValidPassword) {
+      res.status(400);
+      res.send(passwordValidationResult.failedMsg);
+    } else {
+      const hashedPassword = await bcrypt.hash(
+        password,
+        SALT_ROUNDS_FOR_PASSWORD_HASHING
+      );
+
+      const queryToAddNewUser = `
+        INSERT INTO
+            user (username, password, name, gender)
+        VALUES
+            ('${username}', '${hashedPassword}', '${name}', '${gender}');
+        
+        `;
+
+      const addNewUserDBResponse = await twitterCloneDBConnectionObj.run(
+        queryToAddNewUser
+      );
+
+      res.send("User created successfully");
+    } // End of else-part of inner if-statement with condition: (!passwordValidationResult.isValidPassword)
+  } // End of else-part of outer if-statement with condition: (!usernameValidationResult.isNewUser)
+});
 
 module.exports = app;
